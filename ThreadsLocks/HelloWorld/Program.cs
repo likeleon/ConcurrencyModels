04e9d3298d63803504ebbc3deae37db092434379
Sung Ho Kim <likeleon@gmail.com>
2016-09-02 01:46:55 +0900

    ThreadsLock HelloWorld

---
+++ ThreadsLocks/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var thread = new Thread(() => Console.WriteLine("Hello from new thread"));
+            thread.Start();
+            Thread.Yield();
+            Console.WriteLine("Hello from main thread");
+            thread.Join();
+        }
+    }
+}
