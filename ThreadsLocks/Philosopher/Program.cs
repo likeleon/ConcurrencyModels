2733004746e9e58f094bc8339091bac6dfad1851
Sung Ho Kim <likeleon@gmail.com>
2016-09-04 10:51:15 +0900

    PhilosopherMonitor

--- ThreadsLocks/Philosopher/Program.cs
+++ ThreadsLocks/Philosopher/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 
 namespace Philosopher
@@ -9,14 +8,23 @@ namespace Philosopher
     {
         static void Main(string[] args)
         {
-            int numPhilosophers = 5;
-            var chopsticks = Generate(numPhilosophers, i => new Chopstick(i)).ToArray();
-            //var philosophers = Generate(numPhilosophers, i => new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % numPhilosophers])).ToArray();
-            //var philosophers = Generate(numPhilosophers, i => new PhilosopherFixed(i, chopsticks[i], chopsticks[(i + 1) % numPhilosophers])).ToArray();
-            var philosophers = Generate(numPhilosophers, i => new PhilosopherTimeout(i, chopsticks[i], chopsticks[(i + 1) % numPhilosophers])).ToArray();
-            var threads = Generate(numPhilosophers, i => new Thread(() => philosophers[i].Run())).ToList();
-            threads.ForEach(t => t.Start());
-            threads.ForEach(t => t.Join());
+            int numSeats = 5;
+            var table = new Table(numSeats, Create<PhilosopherMonitor>);
+
+            var threads = new Thread[table.Philosophers.Length];
+            foreach (var p in table.Philosophers)
+            {
+                threads[p.Index] = new Thread(() => p.Run());
+                threads[p.Index].Start();
+            }
+            foreach (var t in threads)
+                t.Join();
+        }
+
+        static Philosopher Create<T>(int index, Table table) where T : Philosopher
+        {
+            var constructor = typeof(T).GetConstructor(new[] { typeof(int), typeof(Table) });
+            return constructor.Invoke(new object[] { index, table }) as Philosopher;
         }
 
         static IEnumerable<T> Generate<T>(int count, Func<int, T> generator)
@@ -26,19 +34,44 @@ namespace Philosopher
         }
     }
 
+    class Table
+    {
+        public Chopstick[] Chopsticks { get; }
+        public Philosopher[] Philosophers { get; }
+
+        public Table(int numSeats, Func<int, Table, Philosopher> philosopherCreator)
+        {
+            Chopsticks = new Chopstick[numSeats];
+            for (int i = 0; i < numSeats; ++i)
+                Chopsticks[i] = new Chopstick(i);
+
+            Philosophers = new Philosopher[numSeats];
+            for (int i = 0; i < numSeats; ++i)
+                Philosophers[i] = philosopherCreator(i, this);
+            foreach (var philosopher in Philosophers)
+                philosopher.AllPhilosophersBeSeated();
+        }
+    }
+
     class Philosopher
     {
-        protected readonly int _index;
+        public int Index { get; }
+        protected Table Table { get; }
         protected readonly Chopstick _left;
         protected readonly Chopstick _right;
-        protected readonly Random _random;
+        readonly Random _random;
 
-        public Philosopher(int index, Chopstick left, Chopstick right)
+        public Philosopher(int index, Table table)
         {
-            _index = index;
-            _left = left;
-            _right = right;
-            _random = new Random(_index);
+            Index = index;
+            Table = table;
+            _left = table.Chopsticks[index];
+            _right = table.Chopsticks[(index + 1) % table.Chopsticks.Length];
+            _random = new Random(Index);
+        }
+
+        public virtual void AllPhilosophersBeSeated()
+        {
         }
 
         public void Run(bool tryDeadlock = true)
@@ -52,14 +85,14 @@ namespace Philosopher
             Think();
             lock (_left)
             {
-                Console.WriteLine($"{_index} got {_left}");
+                Console.WriteLine($"{Index} got {_left}");
 
                 if (tryDeadlock)
                     Think(1000);
 
                 lock (_right)
                 {
-                    Console.WriteLine($"{_index} got {_right}");
+                    Console.WriteLine($"{Index} got {_right}");
                     Eat();
                 }
             }
@@ -68,17 +101,17 @@ namespace Philosopher
         protected void Think(int? forceDuration = null)
         {
             var duration = forceDuration ?? _random.Next(1000);
-            Console.WriteLine($"{_index} start thinking {duration}ms");
+            Console.WriteLine($"{Index} start thinking {duration}ms");
             Thread.Sleep(duration);
-            Console.WriteLine($"{_index} ended thinking");
+            Console.WriteLine($"{Index} ended thinking");
         }
 
         protected void Eat()
         {
             var duration = _random.Next(1000);
-            Console.WriteLine($"{_index} start eating {duration}ms");
+            Console.WriteLine($"{Index} start eating {duration}ms");
             Thread.Sleep(duration);
-            Console.WriteLine($"{_index} ended eating {duration}ms");
+            Console.WriteLine($"{Index} ended eating");
         }
     }
 
@@ -87,18 +120,18 @@ namespace Philosopher
         readonly Chopstick _first;
         readonly Chopstick _second;
 
-        public PhilosopherFixed(int index, Chopstick left, Chopstick right)
-           : base(index, left, right)
+        public PhilosopherFixed(int index, Table table)
+           : base(index, table)
         {
-            if (left.Index < right.Index)
+            if (_left.Index < _right.Index)
             {
-                _first = left;
-                _second = right;
+                _first = _left;
+                _second = _right;
             }
             else
             {
-                _first = right;
-                _second = left;
+                _first = _right;
+                _second = _left;
             }
         }
 
@@ -107,25 +140,24 @@ namespace Philosopher
             Think();
             lock (_first)
             {
-                Console.WriteLine($"{_index} got {_first}");
+                Console.WriteLine($"{Index} got {_first}");
 
                 if (tryDeadlock)
                     Think(1000);
 
                 lock (_second)
                 {
-                    Console.WriteLine($"{_index} got {_second}");
+                    Console.WriteLine($"{Index} got {_second}");
                     Eat();
                 }
             }
-
         }
     }
 
     class PhilosopherTimeout : Philosopher
     {
-        public PhilosopherTimeout(int index, Chopstick left, Chopstick right) 
-            : base(index, left, right)
+        public PhilosopherTimeout(int index, Table table) 
+            : base(index, table)
         {
         }
 
@@ -135,14 +167,14 @@ namespace Philosopher
             Monitor.Enter(_left);
             try
             {
-                Console.WriteLine($"{_index} got {_left}");
+                Console.WriteLine($"{Index} got {_left}");
 
                 if (tryDeadlock)
                     Think(1000);
 
                 if (Monitor.TryEnter(_right, TimeSpan.FromSeconds(1)))
                 {
-                    Console.WriteLine($"{_index} got {_right}");
+                    Console.WriteLine($"{Index} got {_right}");
                     try
                     {
                         Eat();
@@ -150,15 +182,63 @@ namespace Philosopher
                     finally
                     {
                         Monitor.Exit(_right);
-                        Console.WriteLine($"{_index} released {_right}");
+                        Console.WriteLine($"{Index} released {_right}");
                     }
                 }
             }
             finally
             {
                 Monitor.Exit(_left);
-                Console.WriteLine($"{_index} released {_left}");
+                Console.WriteLine($"{Index} released {_left}");
             }
+        }
+    }
+
+    class PhilosopherMonitor : Philosopher
+    {
+        public bool Eating { get; private set; }
+
+        PhilosopherMonitor _leftPhilosopher;
+        PhilosopherMonitor _rightPhilosopher;
+
+        public PhilosopherMonitor(int index, Table table)
+            : base(index, table)
+        {
+        }
+
+        public override void AllPhilosophersBeSeated()
+        {
+            _leftPhilosopher = Table.Philosophers[(Index + 4) % Table.Philosophers.Length] as PhilosopherMonitor;
+            _rightPhilosopher = Table.Philosophers[(Index + 1) % Table.Philosophers.Length] as PhilosopherMonitor;
+        }
+
+        protected override void Loop(bool tryDeadlock)
+        {
+            Monitor.Enter(Table);
+            try
+            {
+                Eating = false;
+                Monitor.Pulse(Table);
+            }
+            finally
+            {
+                Monitor.Exit(Table);
+            }
+            Think();
+
+            Monitor.Enter(Table);
+            try
+            {
+                while (_leftPhilosopher.Eating || _rightPhilosopher.Eating)
+                    Monitor.Wait(Table);
+
+                Eating = true;
+            }
+            finally
+            {
+                Monitor.Exit(Table);
+            }
+            Eat();
         }
     }
 
