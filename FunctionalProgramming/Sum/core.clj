c365254a12a0663e316b29d14ccf8669a51df9a5
Sung Ho Kim <likeleon@gmail.com>
2016-09-11 20:46:35 +0900

    Clojure "#()" reader macro

--- FunctionalProgramming/Sum/core.clj
+++ FunctionalProgramming/Sum/core.clj
@@ -1,3 +1,9 @@
 (defn recur-sum [coll result]
   (if (empty? coll) result
     (recur (rest coll) (+ (first coll) result))))
+
+(defn reduce-sum [coll]
+  (reduce (fn [acc x] (+ acc x)) 0 coll))
+
+(defn reduce-sum2 [coll]
+  (reduce #(+ % %2) 0 coll))
