b91dfb48198caf72a1ac6fdee4da9e462e142d42
Sung Ho Kim <likeleon@gmail.com>
2016-09-18 01:33:16 +0900

    clojure reduce

---
+++ FunctionalProgramming/Reducers/src/reducers/core.clj
@@ -0,0 +1,25 @@
+(ns reducers.core
+  (:require [clojure.core.protocols :refer [CollReduce coll-reduce]]
+            [clojure.core.reducers :refer [CollFold coll-fold]]))
+
+(defn my-reduce
+  ([f coll] (coll-reduce coll f))
+  ([f init coll] (coll-reduce coll f init)))
+
+(defn make-reducer [reducible transformf]
+  (reify
+    CollReduce
+	(coll-reduce [_ f1]
+	  (coll-reduce reducible (transformf f1) (f1)))
+	(coll-reduce [_ f1 init]
+	  (coll-reduce reducible (transformf f1) init))))
+
+(defn my-map [mapf reducible]
+  (make-reducer reducible
+    (fn [reducef]
+	  (fn [acc v]
+	    (reducef acc (mapf v))))))
+
+(defn -main [& args]
+  (print (into [] (my-map (partial * 2) [1 2 3 4])))
+  (shutdown-agents))
