3acbdc008cb114a88f15448b86a9863a488f1602
Sung Ho Kim <likeleon@gmail.com>
2016-10-22 01:57:16 +0900

    2장 연습문제 my-mapcat은 못 하겠다

--- FunctionalProgramming/exercises/src/exercises/core.clj
+++ FunctionalProgramming/exercises/src/exercises/core.clj
@@ -1,4 +1,6 @@
-(ns exercises.core)
+(ns exercises.core
+  (:require [clojure.core.protocols :refer [CollReduce coll-reduce]]
+            [clojure.core.reducers :refer [CollFold coll-fold]]))
 
 ; day 1
 (defn recursive-sum-recur [numbers]
@@ -10,3 +12,34 @@
 
 (defn reduce-sum [numbers]
   (reduce #(+ %1 %2) 0 numbers))
+
+; day 2
+(defn make-reducer [reducible transformf]
+  (reify
+    CollReduce
+    (coll-reduce [_ f1]
+      (coll-reduce reducible (transformf f1) (f1)))
+    (coll-reduce [_ f1 init]
+      (coll-reduce reducible (transformf f1) init))))
+
+(defn my-map [mapf reducible]
+  (make-reducer reducible
+                (fn [reducef]
+                  (fn [acc v]
+                    (reducef acc (mapf v))))))
+
+(defn my-flatten [reducible]
+  (make-reducer reducible
+                (fn [reducef]
+                  (fn [acc v]
+                    (if (sequential? v)
+                      (coll-reduce (my-flatten v) reducef acc)
+                      (reducef acc v))))))
+
+(defn my-filter [filterf reducible]
+  (make-reducer reducible
+                (fn [reducef]
+                  (fn [acc v]
+                    (if (filterf v)
+                      (reducef acc v)
+                      acc)))))
